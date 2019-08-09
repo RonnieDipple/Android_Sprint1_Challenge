c4b9e232cc826d156d8a4336a43d2bd27527734f
ronniedipple <ronniedipple@gmail.com>
2019-08-09 12:17:58 -0500

    problem creating textview text

---
+++ Android_Sprint1_Challenge/app/src/main/java/com/example/android_sprint1_challenge/ListActivity.kt
@@ -0,0 +1,51 @@
+package com.example.android_sprint1_challenge
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_list.*
+
+class ListActivity : AppCompatActivity() {
+
+    companion object{
+        const val ADD_TITLE_CODE = 69
+    }
+
+    private val titleList = mutableListOf<TitleData>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+        btn_add_movie.setOnClickListener {
+            val intent = Intent (this, EditActivity::class.java)
+            startActivityForResult(intent, ADD_TITLE_CODE)
+        }
+    }
+
+    fun createTextView(titleData: TitleData, index: Int): TextView{
+        val textView = TextView(this)
+        textView.textSize = 15f
+        //TODO may display title wrong
+        textView.text = titleData
+
+        return textView
+    }
+
+    fun populateList(index: Int){
+        movie_list_layout.removeAllViews()
+        for (titleData in titleList){
+            movie_list_layout.addView(createTextView(titleData, index))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == ADD_TITLE_CODE && resultCode == Activity.RESULT_OK){
+            val titleData = data?.getSerializableExtra(TitleData.TITLE_TAG) as TitleData
+            titleList.add(titleData)
+            populateList(titleList.size-1)
+        }
+    }
+}
